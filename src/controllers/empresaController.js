a72bc741a687f3cabcab5bef3c66589ba3714453
Erick <erick.miyagi@sptech.school>
2025-03-08 16:28:48 -0300

    Modificando a função cadastrarEmpresa
    Colocamos as 3 funções para serem chamadas em uma then

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -1,4 +1,5 @@
 var empresaModel = require("../models/empresaModel");
+var usuarioModel = require("../models/usuarioModel");
 
 function buscarPorCnpj(req, res) {
   var cnpj = req.query.cnpj;
@@ -27,7 +28,18 @@ function cadastrarEmpresa(req, res) {
   var numeroTin = req.body.numeroTinServer;
   var status = req.body.statusServer;
   var telefone = req.body.telefoneServer;
-  var fkEndereco = req.body.idEnderecoSede;
+  var site = req.body.siteServer;
+
+  var cep = req.body.cepServer;
+  var logradouro = req.body.logradouroServer;
+  var numero = req.body.numeroServer;
+  var complemento = req.body.complementoServer;
+
+  var nome = req.body.nomeServer;
+  var cpf = req.body.cpfServer;
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
+  var permissao = req.body.permissaoServer;
 
   empresaModel.buscarPorCnpj(razaoSocial).then((resultado) => {
     if (resultado.length > 0) {
@@ -35,29 +47,24 @@ function cadastrarEmpresa(req, res) {
         .status(401)
         .json({ mensagem: `a empresa com o cnpj ${razaoSocial} já existe` });
     } else {
-      empresaModel.cadastrarEmpresa(razaoSocial, numeroTin, status, telefone, site, fkEndereco).then((resultado) => {
-        res.status(201).json(resultado);
+
+      empresaModel.cadastrarEmpresa(razaoSocial, numeroTin, status, telefone, site).then((resultadoEmpresa) => {
+
+        empresaModel.enviarEndereco(cep, logradouro, numero, complemento, resultadoEmpresa.fkEmpresa).then((resultadoEndereco) => {
+        });
+        usuarioModel.cadastrar(nome, cpf, email, senha,permissao="admin", empresaModel.enviarEndereco().fkEmpresa).then((resultadoFuncionario) => {
+          res.status(201).json(resultadoFuncionario);
+        }).catch(function (){
+          res.status(400).json("Erro de cadastro")
+        });
       });
     }
   });
-}
-
-function enviarEndereco(req, res){
-  var cep = req.body.cepServer;
-  var logradouro = req.body.logradouroServer;
-  var numero = req.body.numeroServer;
-  var complemento = req.body.complementoServer;
-
-  empresaModel.enviarEndereco(cep, logradouro, numero, complemento).then((resultado) => {
-    res.status(201).json(resultado);
-  });
-
 }
 
 module.exports = {
   buscarPorCnpj,
   buscarPorId,
   cadastrarEmpresa,
-  enviarEndereco,
   listar,
 };
--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,33 +1,38 @@
 var database = require("../database/config");
 
-function buscarPorId(id) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
+function buscarPorId(idEmpresa) {
+  var instrucaoSql = `SELECT * FROM empresa WHERE idEmpresa = '${idEmpresa}'`;
 
   return database.executar(instrucaoSql);
 }
 
 function listar() {
-  var instrucaoSql = `SELECT id, razao_social, cnpj, codigo_ativacao FROM empresa`;
+  var instrucaoSql = `SELECT id, razaoSocial, cnpj, codigo_ativacao FROM empresa`;
 
   return database.executar(instrucaoSql);
 }
 
-function buscarPorCnpj(cnpj) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
+function buscarPorCnpj(razaoSocial) {
+  var instrucaoSql = `SELECT * FROM Empresa WHERE razaoSocial = '${razaoSocial}'`;
 
   return database.executar(instrucaoSql);
 }
 
-function cadastrarEmpresa(razaoSocial, numeroTin, status, telefone, site, fkEndereco) {
-  var instrucaoSql = `INSERT INTO Empresa (razao_social, numeroTin, status, telefone, site, fkEndereco) VALUES ('${razaoSocial}', '${cnpj}', '${numeroTin}', '${status}', '${telefone}', '${site}',  '${fkEndereco}')`;
+function cadastrarEmpresa(razaoSocial, numeroTin, status, telefone, site) {
+  var instrucaoSql = `INSERT INTO Empresa (razaoSocial, numeroTin, status, telefone, site) VALUES ('${razaoSocial}', '${numeroTin}', '${status}', '${telefone}', '${site}')`;
 
   return database.executar(instrucaoSql);
 }
 
-function enviarEndereco(cep, logradouro, numero, complemento){
-  var instrucaoSql = `INSERT INTO EnderecoSede (cep, logradouro, numero, complemento) VALUES ('${cep}', '${logradouro}', '${numero}', '${complemento}', '${telefone}', '${site}',  '${fkEndereco}')`;
+function enviarEndereco(cep, logradouro, numero, complemento, fkEmpresa){
+  var instrucaoSql = `INSERT INTO EnderecoSede (cep, logradouro, numero, complemento, fkEmpresa) VALUES ('${cep}', '${logradouro}', '${numero}', '${complemento}', '${fkEmpresa}')`;
   
   return database.executar(instrucaoSql);
 }
 
-module.exports = { buscarPorCnpj, buscarPorId, cadastrarEmpresa, listar, enviarEndereco };
+module.exports = { 
+  buscarPorCnpj, 
+  buscarPorId, 
+  cadastrarEmpresa, 
+  listar, 
+  enviarEndereco };
