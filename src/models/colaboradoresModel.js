7095c5945db4a0d6290ad98423c7197185877f03
MiguelCSerpa <miguel.serpa@sptech.school>
2025-04-23 08:57:13 -0300

    Organizando redimencionamento de paginas e nomes de acordo com o login

--- src/models/colaboradoresModel.js
+++ src/models/colaboradoresModel.js
@@ -9,7 +9,7 @@ function patchFkEmpresa(idColaborador, fkEmpresa){
 function postAutenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idColaborador, nome, email, fkEmpresa FROM Colaborador WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idColaborador, nome, email, nivel, fkEmpresa FROM Colaborador WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
