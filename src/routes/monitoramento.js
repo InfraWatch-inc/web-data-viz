6dc143a3cda1ff35074a3319473e92f5b48a3c41
KaioCassiano <kaio.barbosa@sptech.school>
2025-04-25 16:42:59 -0300

    Atualizações

--- src/routes/monitoramento.js
+++ src/routes/monitoramento.js
@@ -3,8 +3,26 @@ var router = express.Router();
 
 var monitoramentoController = require("../controllers/monitoramentoController");
 
-router.get("/:idServidor", function (req, res) {
-    monitoramentoController.buscarDadosTempoReal(req, res);
+// router.get("/:idServidor", function (req, res) {
+//     monitoramentoController.buscarDadosTempoReal(req, res);
+// })
+
+router.get("/:uuid", function (req, res) {
+    monitoramentoController.buscarDados(req, res);
 })
+
+router.post("/dados", function (req, res) {
+    monitoramentoController.CadastrarCaptura(req, res);
+})
+
+router.post("/alerta", function (req, res) {
+    monitoramentoController.CadastrarAlerta(req, res);
+})
+
+router.post("/processos", function (req, res) {
+    monitoramentoController.CadastrarProcesso(req, res);
+})
+
+
 
 module.exports = router;
