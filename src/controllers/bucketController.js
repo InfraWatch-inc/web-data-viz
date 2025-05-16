840e8629988be90022bdcba4487112d947c3466d
RanierDalton <ranier.couto@sptech.school>
2025-05-16 11:01:01 -0300

    Reorganizando estrutura do JSON para enviar ao bucket bronze

--- src/controllers/bucketController.js
+++ src/controllers/bucketController.js
@@ -7,15 +7,6 @@ async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// TODO Estuturar dados 
-        
-//{
-//    "servidor":"SRV001",
-//    "dtHora": "",
-//    "isAlerta":true,
-//    "cpu1_uso":20.0
-//}
-
 async function enviar(bucketName){
   const s3Client = new S3Client({region: "us-east-1"});
     while (true){
@@ -24,16 +15,41 @@ async function enviar(bucketName){
         
       dados = monitoramentoController.monitoramento;
 
-      console.log("dados", dados);
       for (const servidor in dados) {
         if (Array.isArray(dados[servidor]) && dados[servidor].length > 30) {
           dados[servidor].splice(0, dados[servidor].length - 30);
         }
       }
 
-      const fileName = `captura-${horarioColeta}.json`;
-      const jsonString = JSON.stringify(dados, null, 2);
+      const ano = horarioColeta.getFullYear();
+      const mes = String(horarioColeta.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+      const dia = String(horarioColeta.getDate()).padStart(2, '0');
+
+      const fileName = `captura-${dia}/${mes}/${ano}.json`;
       
+      
+      for(servidor in dados){
+        let dadosReestruturados = [];
+
+        for(captura in servidor){
+          servidor = `SRV-${captura.idServidor}`;
+
+          dicionario = {
+            "servidor":servidor,
+            "dtHora":captura.dataHora,
+            "isAlerta":true,
+          }
+
+          captura.dadosCaptura.forEach((cap) => {
+            let coluna = `${cap.componente}${cap.numeracao}`; // TODO ADD DESCRICAO DO COMPONENTE 
+            dicionaro[`${coluna}`] = cap.dadoCaptura;
+          });
+          
+          dadosReestruturados.push(dicionario);
+        }
+      }
+
+      const jsonString = JSON.stringify(dadosReestruturados, null, 2);
       try {
         const command = new PutObjectCommand({
           Bucket: bucketName,
@@ -53,5 +69,5 @@ async function enviar(bucketName){
 }
 
 module.exports = {
-    enviar
+  enviar
 }
