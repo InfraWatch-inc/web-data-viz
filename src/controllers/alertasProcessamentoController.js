006e43fc884efcf36a28b7bc669e7620d298ca1c
KaioCassiano <kaio.barbosa@sptech.school>
2025-05-25 19:13:23 -0300

    Filtro funcionando com KPIs

--- src/controllers/alertasProcessamentoController.js
+++ src/controllers/alertasProcessamentoController.js
@@ -1,54 +1,12 @@
 let alertasProcessamentoModel = require("../models/alertasProcessamentoModel");
 
-function getAlertasPorPeriodoTrimestral(req, res) {
+function informacoesAlertas(req, res) {
 
     const idEmpresa = req.body.idEmpresa;
-
-    alertasProcessamentoModel.getQtdAlertasPeriodoTrimestral(idEmpresa)
-
-        .then(
-            function (resultado) {
-                res.json(resultado);
-            }
-        ).catch(
-            function (erro) {
-                console.log(erro);
-                console.log(
-                    "\nHouve um erro ao buscar dados do usuario ",
-                    erro.sqlMessage
-                );
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-}
-
-function getAlertasPorPeriodoSemestral(req, res) {
-
-    const idEmpresa = req.body.idEmpresa;
-
-    alertasProcessamentoModel.getQtdAlertasPeriodoSemestral(idEmpresa)
-
-        .then(
-            function (resultado) {
-                res.json(resultado);
-            }
-        ).catch(
-            function (erro) {
-                console.log(erro);
-                console.log(
-                    "\nHouve um erro ao buscar dados do usuario ",
-                    erro.sqlMessage
-                );
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-}
-
-function getAlertasPorPeriodoAnual(req, res) {
-
-    const idEmpresa = req.body.idEmpresa;
-
-    alertasProcessamentoModel.getQtdAlertasPeriodoAnual(idEmpresa)
+    const idEscolhaTempo = req.body.idEscolhaTemporal;
+    
+    console.log(idEmpresa, idEscolhaTempo);
+    alertasProcessamentoModel.getInformacoesAlertas(idEmpresa, idEscolhaTempo)
 
         .then(
             function (resultado) {
@@ -67,74 +25,7 @@ function getAlertasPorPeriodoAnual(req, res) {
 }
 
 
-function getComponenteMaiorQtdAlertaCriticoTrimestral(req, res) {
-
-     const idEmpresa = req.body.idEmpresa;
-
-    alertasProcessamentoModel.getComponenteMaiorQtdAlertaCriticoTrimestral(idEmpresa)
-        .then(
-            function (resultado) {
-                res.json(resultado);
-            }
-        ).catch(
-            function (erro) {
-                console.log(erro);
-                console.log(
-                    "\nHouve um erro ao buscar dados do usuario ",
-                    erro.sqlMessage
-                );
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-}
-
-function getComponenteMaiorQtdAlertaCriticoSemestral(req, res) {
-
-     const idEmpresa = req.body.idEmpresa;
-
-    alertasProcessamentoModel.getComponenteMaiorQtdAlertaCriticoSemestral(idEmpresa)
-        .then(
-            function (resultado) {
-                res.json(resultado);
-            }
-        ).catch(
-            function (erro) {
-                console.log(erro);
-                console.log(
-                    "\nHouve um erro ao buscar dados do usuario ",
-                    erro.sqlMessage
-                );
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-}
-
-function getComponenteMaiorQtdAlertaCriticoAnual(req, res) {
-
-     const idEmpresa = req.body.idEmpresa;
-
-    alertasProcessamentoModel.getComponenteMaiorQtdAlertaCriticoAnual(idEmpresa)
-        .then(
-            function (resultado) {
-                res.json(resultado);
-            }
-        ).catch(
-            function (erro) {
-                console.log(erro);
-                console.log(
-                    "\nHouve um erro ao buscar dados do usuario ",
-                    erro.sqlMessage
-                );
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-}
 
 module.exports = {
-    getComponenteMaiorQtdAlertaCriticoTrimestral,
-    getComponenteMaiorQtdAlertaCriticoSemestral,
-    getComponenteMaiorQtdAlertaCriticoAnual,
-    getAlertasPorPeriodoTrimestral,
-    getAlertasPorPeriodoSemestral,
-    getAlertasPorPeriodoAnual   
+    informacoesAlertas
 }
--- src/routes/alertasProcessamento.js
+++ src/routes/alertasProcessamento.js
@@ -3,28 +3,8 @@ var router = express.Router();
 
 var alertasProcessamentoController = require("../controllers/alertasProcessamentoController");
 
-router.post("/AlertasPorPeriodoTrimestral", function (req, res){
-alertasProcessamentoController.getAlertasPorPeriodoTrimestral(req, res);
-});
-
-router.post("/AlertasPorPeriodoSemestral", function (req, res){
-alertasProcessamentoController.getAlertasPorPeriodoSemestral(req, res);
-});
-
-router.post("/AlertasPorPeriodoAnual", function (req, res){
-alertasProcessamentoController.getAlertasPorPeriodoAnual(req, res);
-});
-
-router.post("/MaiorQtdAlertaCriticoTrimestral", function (req, res){
-alertasProcessamentoController.getComponenteMaiorQtdAlertaCriticoTrimestral(req, res);
-});
-
-router.post("/MaiorQtdAlertaCriticoSemestral", function (req, res){
-alertasProcessamentoController.getComponenteMaiorQtdAlertaCriticoSemestral(req, res);
-});
-
-router.post("/MaiorQtdAlertaCriticoAnual", function (req, res){
-alertasProcessamentoController.getComponenteMaiorQtdAlertaCriticoAnual(req, res);
+router.post("/informacoesAlertas", function (req, res){
+alertasProcessamentoController.informacoesAlertas(req, res);
 });
 
 module.exports = router;
