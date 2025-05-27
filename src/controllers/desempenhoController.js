ead5e7c1c30eea122360ed68b92d6b7a4100c9e9
ViMoraes04 <vitoria.moraes@sptech.school>
2025-05-27 15:34:17 -0300

    back desempenho

--- src/controllers/desempenhoController.js
+++ src/controllers/desempenhoController.js
@@ -1,20 +1,29 @@
 var desempenhoModel = require("../models/desempenhoModel");
 
-function getChamado(req, res) {
-    desempenhoModel.getChamado()
-        .then(resposta => {
-            if (!resposta) {
-                return res.status(500).json({ erro: "Erro ao obter chamados do Jira" });
-            }
-            res.status(200).json(resposta);
-        })
-        .catch(erro => {
-            console.error("Erro no controller getChamado:", erro);
-            res.status(500).json({ erro: "Erro interno ao buscar chamados" });
-        });
+// function getChamado(req, res) {
+//     desempenhoModel.getChamado()
+//         .then(resposta => {
+//             if (!resposta) {
+//                 return res.status(500).json({ erro: "Erro ao obter chamados do Jira" });
+//             }
+//             res.status(200).json(resposta);
+//         })
+//         .catch(erro => {
+//             console.error("Erro no controller getChamado:", erro);
+//             res.status(500).json({ erro: "Erro interno ao buscar chamados" });
+//         });
+// }
+
+function receberChamado(req,res){
+    const dados = req.body;
+    console.log("json recebido do python", dados);
+    console.log(req.body);
+    
+    res.status(200).json({mensagem: "Chamado recebido"});
+    
 }
 
-
 module.exports = {
-    getChamado
+    // getChamado,
+    receberChamado
 }
--- src/routes/desempenho.js
+++ src/routes/desempenho.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 var desempenhoController = require("../controllers/desempenhoController");
 
-router.get("/buscar/chamado", function (req, res){
-    desempenhoController.getChamado(req, res)
-})
+// router.get('/desempenho/buscar/chamado', desempenhoController.getChamado)
+
+router.get('/desempenho/buscar/chamado', desempenhoController.receberChamado)
 
 module.exports = 
 router;
