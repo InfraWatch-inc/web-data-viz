a937fb209fd1b48046ff399f5cadbe6df74d1102
MiguelCSerpa <miguel.serpa@sptech.school>
2025-06-02 11:03:19 -0300

    ajustando script conexao bucket para aws

--- src/models/bucketOuroExternoModel.js
+++ src/models/bucketOuroExternoModel.js
@@ -6,9 +6,6 @@ const bucket = process.env.S3_BUCKET;
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        sessionToken: process.env.AWS_SESSION_TOKEN
     }
 });
 
